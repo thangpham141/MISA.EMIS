07be2ded026ac3c78feeae8a42bf76e05e83c381
thangpham141 <14012000phamngocthang@gmail.com>
2021-02-23 14:45:56 +0700

    push code EMIS

---
+++ MISA.FeeManagement/MISA.FeeManagement.Api/Controllers/BasesController.cs
@@ -0,0 +1,103 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MISA.Service.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MISA.FeeManagement.Api.Controllers
+{
+    [Route("api/v1/[controller]")]
+    [ApiController]
+    public class BasesController<TEntity> : ControllerBase
+    {
+        #region DECLARE
+        /// <summary>
+        /// Khởi tạo Interface
+        /// </summary>
+        IBaseService<TEntity> _baseService;
+        #endregion
+        #region CONTRUCTOR
+        /// <summary>
+        /// Khởi tạo truyền tham số cho Interface
+        /// </summary>
+        /// <param name="baseService"></param>
+        public BasesController(IBaseService<TEntity> baseService)
+        {
+            _baseService = baseService;
+        }
+        #endregion
+        #region METHODS
+        /// <summary>
+        /// Phương thức Get để lấy dữ liệu
+        /// </summary>
+        /// <returns>Statuscode và data</returns>
+        /// CreatedBy: PNTHANG(22/2/2021)
+        [HttpGet]
+        public IActionResult Get()
+        {
+            var result = _baseService.GetData();
+            var datas = (List<TEntity>)result.Data;
+            if (datas.Count == 0)
+            {
+                return StatusCode(204, result.Data);
+            }
+
+            return StatusCode(200, result.Data);
+        }
+        /// <summary>
+        /// Phương thức Post để thêm mới dữ liệu
+        /// </summary>
+        /// <param name="entity">Đối tượng cần thêm</param>
+        /// <returns>StatusCode và số bản dữ liệu thêm</returns>
+        /// CreatedBy: PNTHANG(22/2/2021)
+        [HttpPost]
+        public IActionResult Post([FromBody] TEntity entity)
+        {
+            var result = _baseService.Post(entity);
+            if (result.Success && (int)result.Data > 0)
+            {
+                return StatusCode(201, result.Data);
+            }
+            else if (result.Success)
+                return StatusCode(204, result.Data);
+            else
+                return StatusCode(400, result.Data);
+        }
+        /// <summary>
+        /// Cập nhật đối tượng
+        /// </summary>
+        /// <param name="entity">đối tượng sửa</param>
+        /// <param name="entityCode">Mã đối tượng</param>
+        /// <returns>StatusCode và số bản dữ liệu sửa</returns>
+        /// CreatedBy: PNTHANG(22/2/2021)
+        [HttpPut]
+        public IActionResult Put([FromBody] TEntity entity, [FromQuery] string entityCode = null)
+        {
+            var result = _baseService.Put(entity, entityCode);
+            if (result.Success && (int)result.Data > 0)
+            {
+                return StatusCode(201, result.Data);
+            }
+            else if (result.Success)
+                return StatusCode(204, result.Data);
+            else
+                return StatusCode(400, result.Data);
+        }
+        /// <summary>
+        /// Xóa đối tượng
+        /// </summary>
+        /// <param name="entityId">ID của đối tượng</param>
+        /// <returns>StatusCode và số bản dữ liệu xóa</returns>
+        [HttpDelete]
+        public IActionResult Delete([FromQuery] string entityId)
+        {
+            var result = _baseService.Delete(entityId);
+            if ((int)result.Data > 0)
+                return StatusCode(200, result.Data);
+            return StatusCode(204, result.Data);
+        }
+        #endregion
+    }
+}
---
+++ MISA.FeeManagement/MISA.Service/Interfaces/IBaseService.cs
@@ -0,0 +1,31 @@
+﻿using MISA.Common.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MISA.Service.Interfaces
+{
+    public interface IBaseService<TEntity>
+    {
+        /// <summary>
+        /// Lấy dữ liệu
+        /// </summary>
+        /// <returns>kết quả thành công - không thành công, dữ liệu , MISA Code(hiện chưa có gì)</returns>
+        /// CreatedBy: PNTHANG(22/2/2021)
+        ServiceResult GetData();
+        /// <summary>
+        /// Thêm dữ liệu
+        /// </summary>
+        /// <param name="entity">đối tượng cần thêm</param>
+        /// <returns>trạng thái thêm: thành công - không thành công, dữ liệu</returns>
+        /// CreatedBy: PNTHANG(22/2/2021)
+        ServiceResult Post(TEntity entity);
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="entityId"></param>
+        /// <returns></returns>
+        ServiceResult Delete(string entityId);
+        ServiceResult Put(TEntity entity, string entityCode = null);
+    }
+}
